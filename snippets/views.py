6df11956e0ed83aa779db409648b2d3505d82313
Siva <Siva@Mulagalas-MacBook-Pro.local>
2018-06-10 15:40:15 +0530

    Added class based views with generics

--- snippets/views.py
+++ snippets/views.py
@@ -10,42 +10,78 @@ from snippets.serializers import SnippetSerializer
 # Create your views here.
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
-	"""
-	This is for listing all the snippets or 
-	for creating new snippet
-	"""
-	if request.method == 'GET':
-		snippets = Snippet.objects.all()
-		serializer = SnippetSerializer(snippets, many=True)
-		return Response(serializer.data)
+    """
+    This is for listing all the snippets or 
+    for creating new snippet
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
 
-	if request.method == 'POST':
-		serializer = SnippetSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def snippet_detail(request, pk, format=None):
-	"""This method is for retrieving, Updating 
-	   and for deleting the snippet.
-	"""
-	try:
-		snippet = Snippet.objects.get(pk=pk)
-	except Snippet.DoesNotExist:
-		print 'in except clause'
-		return Response(status = status.HTTP_404_NOT_FOUND)
+    """This method is for retrieving, Updating 
+       and for deleting the snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        print 'in except clause'
+        return Response(status = status.HTTP_404_NOT_FOUND)
 
-	if request.method == 'GET':
-		serializer = SnippetSerializer(snippet)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-	if request.method == 'PUT':
-		serializer = SnippetSerializer(data = request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status = status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	if request.method == 'DELETE':
-		snippet.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == 'PUT':
+        serializer = SnippetSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+#Class based views
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+
+class SnippetList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
